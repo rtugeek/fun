4f42a80a705cec133a7d74461dc968a786018fce
Neo Fu <rtugeek@gmail.com>
2024-06-19 15:00:46 +0800

    初版功能

--- src/widgets/wooden-fish/WoodenFish.widget.ts
+++ src/widgets/wooden-fish/WoodenFish.widget.ts
@@ -1,17 +1,14 @@
 import { Widget, WidgetKeyword } from '@widget-js/core';
-//TODO 修改组件信息，标题，描述，关键词
+
 const WoodenFishWidget = new Widget({
   name: 'widget.js.fun.wooden_fish',
   title: { 'zh-CN': '电子木鱼' },
-  description: { 'zh-CN': '' },
+  description: { 'zh-CN': '修电子福报' },
   keywords: [WidgetKeyword.RECOMMEND],
   lang: 'zh-CN',
-  width: 4,
-  height: 4,
-  minWidth: 4,
-  maxWidth: 4,
-  minHeight: 4,
-  maxHeight: 4,
+  width: 2,
+  height: 2,
+  categories:['fun'],
   previewImage: '修改为组件预览图地址',
   path: '/widget/wooden_fish',
   configPagePath: '/widget/config/wooden_fish',
--- src/widgets/wooden-fish/WoodenFishWidgetView.vue
+++ src/widgets/wooden-fish/WoodenFishWidgetView.vue
@@ -1,14 +1,99 @@
 <template>
   <widget-wrapper>
-    <span>Hello World!</span>
+    <div class="root">
+      <Music class="bgm" :class="{active:playBgm}" @click="onBgmClick"/>
+      <img src="@/assets/images/wooden_fish.png" @click="hit" class="fish">
+    </div>
+    <audio src="./audio/hit.mp3" ref="hitRef"></audio>
+    <audio src="./audio/bgm.mp3" ref="bgmRef"></audio>
   </widget-wrapper>
 </template>
 
 <script lang="ts" setup>
 import { WidgetData } from '@widget-js/core';
 import { useWidget } from '@widget-js/vue3';
+import { Music } from '@icon-park/vue-next';
+import { ref } from 'vue'
+import { useStorage } from '@vueuse/core'
 
-const { widgetData, widgetParams } = useWidget(WidgetData);
+useWidget(WidgetData);
+const playBgm = useStorage('play-music',false)
+const hitRef = ref<HTMLAudioElement>()
+const bgmRef = ref<HTMLAudioElement>()
+
+const keyframes = [
+  { transform: 'translateY(0)',opacity: 1 },
+  { transform: 'translateY(-40px)',opacity: 0 }
+]
+
+const animationOptions = {
+  duration: 1000,
+  iterations: 1,
+}
+const hit = ()=>{
+  const node = hitRef.value?.cloneNode() as HTMLAudioElement
+  node.play()
+
+  const merits = document.createElement('div')
+  merits.classList.add('merits')
+  merits.innerText = '功德+1'
+  const rootElement = document.querySelector('.root')
+  rootElement?.appendChild(merits)
+  merits.animate(keyframes,animationOptions)
+
+  setTimeout(()=>{
+    rootElement?.removeChild(merits)
+  }, 1000)
+}
+
+const onBgmClick = ()=>{
+  playBgm.value = !playBgm.value
+  if (playBgm.value) {
+    bgmRef.value?.play()
+  } else {
+    bgmRef.value?.pause()
+  }
+}
+
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.root{
+  background-color: var(--widget-background-color);
+  border-radius: var(--widget-border-radius);
+  color: var(--widget-color);
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  .merits {
+    position: absolute;
+    right: 15vw;
+    top: 20vh;
+    color: white;
+    font-size: 1.2rem;
+  }
+  .bgm{
+    position: absolute;
+    top: 8px;
+    left: 8px;
+    font-size: 1.2rem;
+    opacity: 0.2;
+    cursor: pointer;
+    &.active{
+      opacity: 1;
+    }
+  }
+
+  .fish{
+    width: 70vw;
+    height: 70vh;
+    cursor: pointer;
+    user-select: none;
+    transition: all 0.3s ease-out;
+    &:active{
+      scale: 0.9;
+    }
+  }
+}
+</style>
